1a1c7f2fcc51011c76c82e1643e841807b786607
lit-alan <Alan.Ryan@lit.ie>
2023-11-20 21:39:11 +0000

    Updated the code to display records of the title table neatly in the console.This version prints the contents of the ResultSet to the console neatly as before, but will only print a max of 50 characters for the title (47 chars + ...). The code has been tidied up as well.

--- Source/Lectures/L9 Using JDBC/L9_Source/src/main/java/sd3/com/Formatting/DynamicTableExample.java
+++ Source/Lectures/L9 Using JDBC/L9_Source/src/main/java/sd3/com/Formatting/DynamicTableExample.java
@@ -2,75 +2,109 @@ package sd3.com.Formatting;
 
 import java.sql.*;
 
+//This version prints the contents of the ResultSet to the console neatly as before,
+//but will only print a max of 50 characters for the title (47 chars + ...)
+
 public class DynamicTableExample {
     public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/books";
         String user = "root";
         String password = "";
         try {
-            // Establishing a connection
-            Connection conn = DriverManager.getConnection(url, user, password);
 
-            // Creating a statement
-            Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            //create the connection object
+            //ATTN: username and password must be changed depending on the settings on your database server
+            Connection connection = DriverManager.getConnection(url, user, password);
 
-            // Executing a query
-            String sql = "SELECT Title,EditionNumber  FROM titles";
-//            String sql = "SELECT *  FROM titles";
-//            String sql = "SELECT Title,EditionNumber, YearPublished, Price  FROM titles";
-            ResultSet rs = stmt.executeQuery(sql);
+            //create a statement object.
+            //We will use this object to carry our query to the database
+            Statement stmt = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+
+            //three sample queries to test the display
+            String q1 = "SELECT * from titles";
+            String q2 = "SELECT Title, EditionNumber, YearPublished, Price from titles";
+            String q3 = "SELECT Title, Price from titles";
+
+            ResultSet rs = stmt.executeQuery(q2);
+
             ResultSetMetaData rsmd = rs.getMetaData();
             int columnCount = rsmd.getColumnCount();
 
-            // Calculate column widths
+            // Determine column widths based on the data in teh ResultSet
             int[] columnWidths = new int[columnCount];
+
+            //titleColumnIndex will ne set to -1 if the title col isnt found in the ResultSet
+            int titleColumnIndex = -1;
+
             for (int i = 1; i <= columnCount; i++) {
-                columnWidths[i - 1] = rsmd.getColumnName(i).length();
+                String columnName = rsmd.getColumnName(i);
+                columnWidths[i - 1] = columnName.length();
+                if ("Title".equalsIgnoreCase(columnName)) {
+                    titleColumnIndex = i;
+                }
             }
+
+            // Check for the width of each row and update columnWidths if necessary
             while (rs.next()) {
                 for (int i = 1; i <= columnCount; i++) {
-                    if (rs.getString(i) != null) {
-                        columnWidths[i - 1] = Math.max(columnWidths[i - 1], rs.getString(i).length());
+                    String value = rs.getString(i);
+                    if (value != null) {
+                        if (i == titleColumnIndex && value.length() > 50) {
+                            value = value.substring(0, 47) + "...";
+                        }
+                        columnWidths[i - 1] = Math.max(columnWidths[i - 1], value.length());
                     }
                 }
             }
+
+            if (titleColumnIndex == -1) {
+                throw new SQLException("Title column not found in the result set");
+            }
+
+            // Reset cursor for printing
             rs.beforeFirst();
 
-            // Print table header
-            String separatorLine = "+";
-            String headerLine = "|";
-            for (int i = 0; i < columnWidths.length; i++) {
-                separatorLine += String.format("%-" + (columnWidths[i] + 2) + "s", "").replace(' ', '-') + "+";
-                headerLine += String.format(" %-"+ columnWidths[i] +"s |", rsmd.getColumnName(i + 1));
-            }
-            System.out.println(separatorLine);
-            System.out.println(headerLine);
-            System.out.println(separatorLine);
+            // Printing the table
+            printTableHeader(rsmd, columnWidths);
+            printTableSeparator(columnWidths);
 
-            // Print rows
-            String rowFormat = "|";
-            for (int width : columnWidths) {
-                rowFormat += " %-" + width + "s |";
-            }
             while (rs.next()) {
-                System.out.format(rowFormat + "\n", (Object[]) getRowValues(rs, columnCount));
+                printRow(rs, columnWidths, titleColumnIndex);
             }
-            System.out.println(separatorLine);
 
-            // Close resources
-            rs.close();
-            stmt.close();
-            conn.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
+            printTableSeparator(columnWidths);
+
+        }//end try
+        catch (Exception e) {
+            System.out.println(e);
         }
     }
 
-    private static Object[] getRowValues(ResultSet rs, int columnCount) throws SQLException {
-        Object[] values = new Object[columnCount];
-        for (int i = 1; i <= columnCount; i++) {
-            values[i - 1] = rs.getString(i) != null ? rs.getString(i) : "";
+    private static void printTableHeader(ResultSetMetaData rsmd, int[] columnWidths) throws SQLException {
+        for (int i = 1; i <= columnWidths.length; i++) {
+            System.out.printf("| %-" + columnWidths[i - 1] + "s ", rsmd.getColumnName(i));
         }
-        return values;
+        System.out.println("|");
+    }
+
+    private static void printRow(ResultSet rs, int[] columnWidths, int titleColumnIndex) throws SQLException {
+        for (int i = 1; i <= columnWidths.length; i++) {
+            String value = rs.getString(i);
+            if (value != null && i == titleColumnIndex && value.length() > 50) {
+                value = value.substring(0, 47) + "...";
+            }
+            System.out.printf("| %-" + columnWidths[i - 1] + "s ", value != null ? value : "");
+        }
+        System.out.println("|");
+    }
+
+    private static void printTableSeparator(int[] columnWidths) {
+        for (int width : columnWidths) {
+            System.out.print("+");
+            for (int i = 0; i < width + 2; i++) {
+                System.out.print("-");
+            }
+        }
+        System.out.println("+");
     }
 }
